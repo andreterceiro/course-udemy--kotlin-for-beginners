fad8dfa28b3a45b2da8ce1ab66a35d7655779e1b
André de Paula Terceiro <andre@terceiro.com.br>
2025-01-14 21:13:42 -0300

    feat: interpolation test

---
+++ 02-Interpolation.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var name: String = "Andre"
+    var age: Int = 43
+    var text1 = "${name}: ${age}"
+    var text2:String = "${name}: ${age}"
+    println(text1)
+    println(text2)
+}
