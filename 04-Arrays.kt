a645e7948e1ca77d5df30858a5bd834bd9b674c0
André de Paula Terceiro <andre@terceiro.com.br>
2025-01-14 22:09:03 -0300

    feat: a syntax that do not work

--- 04-Arrays.kt
+++ 04-Arrays.kt
@@ -1,4 +1,8 @@
 fun main() {
     var names1 = arrayOf("Júlio", "Enzo", "Thais")
     print(names1.contentToString())
+
+    // Do not work
+    // var names2 = {"Benedito", "Rodrigo", "Hygor"}
+    // print(names2.contentToString())
 }
